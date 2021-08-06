b52abb0634147313487f171ed9ca53cc56a55db5
Oknolaz <oknolaz.freedom@protonmail.com>
2021-08-06 11:00:06 +0300

    v0.1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# guess_the_animal_skill
-Gaming skill for Vasisualy's voice assistant that comes out the animal to be guess.
+# Guess the animal game
+Gaming skill for [Vasisualy's voice assistant](https://github.com/Oknolaz/vasisualy) that comes out the animal to be guess. When activating the skill, intercepts the input of the user, to exit it - guess the animal or enter the exit trigger.
+## Supported platforms
+- **GNU/Linux**
+- **Windows**
+## Installation
+Clone this repo as a `picture_of_the_day` and move it to `vasisualy/skills` dir:
+```
+git clone https://github.com/Oknolaz/guess_the_animal_skill guess_the_animal
+```
+## Example
+For start say or text to [Vasisualy](https://github.com/Oknolaz/vasisualy): **"Давай сыграем в угадай животное"**.
+If you need help in guessing: **"Подсказка"**
+For exit: **"Выход"**
---
+++ __init__.py
@@ -0,0 +1,94 @@
+from vasisualy.skills.vas_skill.vas_skill import Skill  # Импорт родительского класса навыков
+import os
+import random
+from . import animals_descs
+
+animals = animals_descs.animals
+animal_naming = ("Оно", "Животное", "Это животное", "Моё животное")
+
+
+class GuessTheAnimal(Skill):
+    def first_run(self, user_message):
+        # Вызывается при активации триггера, затем навык перехватывает ввод пользователя.
+        if super(GuessTheAnimal, self)._is_triggered(user_message, super(GuessTheAnimal, self)._get_triggers()):
+            animals_keys = []
+
+            for i in animals.keys():
+                animals_keys.append(i)
+            animal = random.choice(animals_keys)
+
+            with open("vasisualy/skills/guess_the_animal/.animal", 'w') as f:
+                f.write(animal)
+
+            descriptions = animals[animal].split("/")
+            first_description = random.choice(descriptions)
+            descriptions.remove(first_description)
+            second_description = random.choice(descriptions)
+            descriptions.remove(second_description)
+
+            with open("vasisualy/skills/guess_the_animal/.descriptions", 'w') as f:
+                for description in descriptions:
+                    f.write(description + "\n")
+
+            toSpeak = f"Я загадал какое-то животное, тебе нужно отгадать его по моему описанию. {random.choice(animal_naming)} {first_description} и {second_description}. Если тебе нужна подсказка - скажи \"подсказка\"."
+
+            super(GuessTheAnimal, self).run_loop()  # Активация цикла навыка.
+
+            return toSpeak
+        else:
+            return ''
+
+    def main(self, user_message):
+        # Вызывается, когда навык активирован и существует файл блокировки, перехватывает
+        # все сообщения, введённые пользователем.
+        with open("vasisualy/skills/guess_the_animal/.animal", 'r') as f:
+            animal = f.read()
+
+        if super(GuessTheAnimal, self)._is_triggered_to_exit(user_message, super(GuessTheAnimal, self)._get_exit_triggers()):
+            toSpeak = "Завершение работы навыка..."
+
+            super(GuessTheAnimal, self).exit_loop()  # Завершение цикла.
+
+            return toSpeak
+
+        elif ("подсказка" in user_message) or ("Подсказка" in user_message):
+            with open("vasisualy/skills/guess_the_animal/.descriptions", 'r') as f:
+                descriptions = []
+                lines = f.read()
+
+                if lines:
+                    lines = lines.split("\n")
+                    for line in lines:
+                        if line: descriptions.append(line)
+
+            s_description = random.choice(descriptions) if len(descriptions) else None
+            toSpeak = f"{random.choice(animal_naming)} {s_description}." if s_description else "Подсказок больше нет!"
+            if s_description: descriptions.remove(s_description)
+
+            with open("vasisualy/skills/guess_the_animal/.descriptions", 'w') as f:
+                for description in descriptions:
+                    f.write(description + '\n')
+
+            return toSpeak
+
+        elif (user_message == animal.capitalize()) or (user_message == animal):
+            toSpeak = "Поздравляю, я загадал именно это животное!"
+
+            super(GuessTheAnimal, self).exit_loop()  # Завершение цикла.
+
+            # Удаление временных файлов.
+            os.remove("vasisualy/skills/guess_the_animal/.animal")
+            os.remove("vasisualy/skills/guess_the_animal/.descriptions")
+            return toSpeak
+
+        else:
+            toSpeak = random.choice(("Не правильно я загадал другое животное.", "Нет.", "Неправильно.", "Я загадал не это."))
+            return toSpeak
+
+def main(user_message):
+    skill = GuessTheAnimal("guess_the_animal", user_message, loop=True)  # Вывод сообщения, переданного навыком, пользователю.
+    return skill.first_run(user_message)
+
+def loop(user_message):
+    skill = GuessTheAnimal("guess_the_animal", user_message, loop=True)
+    return skill.main(user_message)
